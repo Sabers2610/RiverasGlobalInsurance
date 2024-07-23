f326ed7a02e12ffca76d1a4ae53607ef4c932255
Sabers2610 <luissaez261002@gmail.com>
2024-07-23 01:25:34 -0400

    arreglado entrada al html y register, falta revisar validaciones de formulario, acepta espacios en blanco y la api dispara bad request

--- API/src/middlewares/adminValidator.middleware.js
+++ API/src/middlewares/adminValidator.middleware.js
@@ -6,7 +6,6 @@ dotenv.config()
 
 export async function adminValidator(req,res,next){
     try {
-
         const authHeader = req.headers['authorization'];
         
         const token = authHeader.split(' ')[1];
@@ -19,13 +18,14 @@ export async function adminValidator(req,res,next){
             return res.status(500).json({ message: 'Secret token is not defined' });
         }
 
+
         const {uid} = jwt.verify(token, jwtSecret)
 
-        const USER = User.findOne({where: {
+
+        const USER = await User.findOne({where: {
             userId: uid,
             userEnabled: true
         }})
-
         if(!USER){
             return res.sendStatus(401)
         }
@@ -41,6 +41,6 @@ export async function adminValidator(req,res,next){
 
         next()
     } catch (error) {
-        return res.status(401).json({ message: error})
+        return res.status(401).json(error)
     }
 }
